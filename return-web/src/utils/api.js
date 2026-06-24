b77811bc3a2272c0073a1666a9bf53fbb8b9f674
Dddiko07 <diko.23285@mhs.unesa.ac.id>
2026-06-25 00:32:22 +0700

    add debug log for scan api

--- return-web/src/utils/api.js
+++ return-web/src/utils/api.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  headers: {
+    "Content-Type": "application/json",
+    // KUNCI UTAMA: Tambahkan ini agar Ngrok tidak memblokir request frontend kamu
+    "ngrok-skip-browser-warning": "true",
+  },
+  withCredentials: true, // Bagus untuk menjaga session/cookies jika diperlukan nanti
 });
 
 api.interceptors.request.use(
