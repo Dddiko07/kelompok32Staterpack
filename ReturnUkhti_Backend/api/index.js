b208800f1ad912c725a2a96b6b1fe486099609c8
Dddiko07 <diko.23285@mhs.unesa.ac.id>
2026-06-24 22:15:50 +0700

    update project

--- ReturnUkhti_Backend/api/index.js
+++ ReturnUkhti_Backend/api/index.js
@@ -1,6 +1,20 @@
-module.exports = (req, res) => {
-    res.status(200).json({
-      success: true,
-      message: "Vercel OK"
-    });
-  };
+require("dns").setDefaultResultOrder("ipv4first");
+require("dotenv").config();
+
+const express = require("express");
+const { connectDB } = require("../src/config/db");
+const authRoutes = require("../src/routes/authRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Running" });
+});
+
+module.exports = app;
